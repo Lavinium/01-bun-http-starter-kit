bd002c0d1a9d948fbca048551ded036e836632fa
finn <burroughszeb@me.com>
2024-08-14 11:39:30 -0500

    wip

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ WORKDIR /usr/src/app
 
 ENV NODE_ENV=production
 
+# TODO: use pre-built binary!
 COPY package*.json bun.lockb ./
 COPY . .
 RUN bun build:server
--- app.ts
+++ app.ts
@@ -1,7 +1,12 @@
 import type { Serve } from "bun";
 
+/**
+ * Provide a configurable app module.
+ */
 export default {
   fetch() {
+    // console.log(Symbol.for("BunServerHMR"));
+    // console.log(globalThis);
     return new Response("Welcome to Bun!");
   },
 } as Serve;
